d3d03373bba81ed253c8669e828fc2521edab62c
basvannoije <basvnoye7@gmail.com>
2024-03-30 21:42:38 +0100

    Created project index.jsx

---
+++ resources/js/Pages/Project/index.jsx
@@ -0,0 +1,14 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+
+
+export default function Index({ auth }) {
+  return (
+    <AuthenticatedLayout
+      user={auth.user}
+      header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+    >
+
+
+    </AuthenticatedLayout>
+  )
+}
