30ff19ea9a437cfe70187595d481c6282f125815
basvannoije <basvnoye7@gmail.com>
2024-04-12 20:45:51 +0200

    Structured jsx better, made the grid work. Added description and tasks table

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -2,8 +2,9 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants";
 
 import { Head } from '@inertiajs/react';
+import TasksTable from "../Task/TasksTable";
 
-export default function Show({ auth, project }) {
+export default function Show({ auth, project, tasks, queryParams }) {
   return (
     <AuthenticatedLayout
       user={auth.user}
@@ -18,63 +19,74 @@ export default function Show({ auth, project }) {
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div>
+              <img src={project.image_path}
+                alt=""
+                className="w-full h-64 object-cover"
+              />
+            </div>
             <div className="p-6 text-gray-900">
-
-              <div>
-                <img src={project.image_path}
-                  alt=""
-                  className="w-full h-64 object-cover"
-                />
-              </div>
               <div className="grid gap-1 grid-cols-2 mt-2">
                 <div>
                   <div>
-                    <div>
-                      <label className="font-bold text-lg">Project ID</label>
-                      <p className="mt-1">{project.id}</p>
-                    </div>
-                    <div className="mt-4">
-                      <label className="font-bold text-lg">Project Name</label>
-                      <p className="mt-1">{project.name}</p>
-                    </div>
-                    <div className="mt-4">
-                      <label className="font-bold text-lg">Project Status</label>
-                      <p className="mt-1">
-                        <span
-                          className={
-                            "px-2 py-1 rounded text-white " +
-                            PROJECT_STATUS_CLASS_MAP[project.status]
-                          }
-                        >
-                          {PROJECT_STATUS_TEXT_MAP[project.status]}
-                        </span>
-                      </p>
-                    </div>
-                    <div className="mt-4">
-                      <label className="font-bold text-lg">
-                        Created By
-                      </ label>
-                      <p>
-                        {project.createdBy.name}
-                      </p>
-                    </div>
+                    <label className="font-bold text-lg">Project ID</label>
+                    <p className="mt-1">{project.id}</p>
                   </div>
+                  <div className="mt-4">
+                    <label className="font-bold text-lg">Project Name</label>
+                    <p className="mt-1">{project.name}</p>
+                  </div>
+                  <div className="mt-4">
+                    <label className="font-bold text-lg">Project Status</label>
+                    <p className="mt-1">
+                      <span
+                        className={
+                          "px-2 py-1 rounded text-white " +
+                          PROJECT_STATUS_CLASS_MAP[project.status]
+                        }
+                      >
+                        {PROJECT_STATUS_TEXT_MAP[project.status]}
+                      </span>
+                    </p>
+                  </div>
+                  <div className="mt-4">
+                    <label className="font-bold text-lg">
+                      Created By
+                    </ label>
+                    <p>
+                      {project.createdBy.name}
+                    </p>
+                  </div>
+                </div>
+                <div>
                   <div>
-                    <div>
-                      <label className="font-bold text-lg">Due Date</label>
-                      <p className="mt-1">{project.due_date}</p>
-                    </div>
-                    <div className="mt-4">
-                      <label className="font-bold text-lg">Create Date</label>
-                      <p className="mt-1">{project.created_at}</p>
-                    </div>
-                    <div className="mt-4">
-                      <label className="font-bold text-lg">updated By</label>
-                      <p className="mt-1">{project.updatedBy.name}</p>
-                    </div>
+                    <label className="font-bold text-lg">Due Date</label>
+                    <p className="mt-1">{project.due_date}</p>
+                  </div>
+                  <div className="mt-4">
+                    <label className="font-bold text-lg">Create Date</label>
+                    <p className="mt-1">{project.created_at}</p>
+                  </div>
+                  <div className="mt-4">
+                    <label className="font-bold text-lg">updated By</label>
+                    <p className="mt-1">{project.updatedBy.name}</p>
                   </div>
                 </div>
               </div>
+              <div className="mt-4">
+                <label className="font-bold text-lg">Project Description</label>
+                <p className="mt-1">{project.description}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div className="p-6 text-gray-900">
+              <TasksTable tasks={tasks} queryParams={queryParams} />
             </div>
           </div>
         </div>
